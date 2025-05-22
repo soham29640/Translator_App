24804db78ab40acce1c56384532bc01288c96d27
soham29640 <soham04x18@gmail.com>
2025-05-22 18:28:39 +0530

    Some changes in .py file

--- src/ui_app.py
+++ src/ui_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+iimport streamlit as st
 from src.translate import load_model, translate_text
 
 st.title("🌍 AI Translator App")
@@ -9,8 +9,16 @@ src_lang = st.selectbox("From Language", ["en", "de", "es", "fr", "it"])
 tgt_lang = st.selectbox("To Language", ["fr", "en", "de", "es", "it"])
 
 if st.button("Translate"):
-    with st.spinner("Translating..."):
-        model, tokenizer = load_model(src_lang, tgt_lang)
-        output = translate_text(text, model, tokenizer)
-        st.success("Translation complete!")
-        st.text_area("Translated Text", output, height=150)
+    if src_lang == tgt_lang:
+        st.warning("Please choose different source and target languages.")
+    else:
+        try:
+            with st.spinner("Translating..."):
+                model, tokenizer = load_model(src_lang, tgt_lang)
+                output = translate_text(text, model, tokenizer)
+                st.success("Translation complete!")
+                st.text_area("Translated Text", output, height=150)
+        except ValueError as ve:
+            st.error(str(ve))
+        except Exception as e:
+            st.error(f"Unexpected error: {e}")
